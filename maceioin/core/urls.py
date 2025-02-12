627195b7f1260b913c0f37b8dc77d73f31374144
Carol <anakfig2016@gmail.com>
2025-02-12 12:58:10 -0300

    add: register page

--- maceioin/core/urls.py
+++ maceioin/core/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from core.views import home, listar, cadastrar, atualizar, editar, deletar
+from core.views import home, listar, adicionar, cadastrar, atualizar, editar, deletar
 
 urlpatterns = [
     path('', home, name="home"),
     path('listar', listar, name="listar"),
+    path('adicionar', adicionar, name="adicionar"),
     path('cadastrar', cadastrar, name="cadastrar"),
-    # Para dizer quer recebe um parâmetro
     path('editar/<int:id>', editar, name="editar"),
     path('atualizar/<int:id>', atualizar, name="atualizar"),
     path('deletar/<int:id>', deletar, name="deletar"),
--- maceioin/core/views.py
+++ maceioin/core/views.py
@@ -9,14 +9,18 @@ def listar(request):
     funcionarios = Funcionario.objects.all()
     return render(request, 'funcionarios.html', {"funcionarios": funcionarios})
 
+def adicionar(request):
+    return render(request, 'cadastrar_funcionario.html')
+
 def cadastrar(request):
     nome = request.POST.get('nome')
     email = request.POST.get('email')
     setor = request.POST.get('setor')
     
-    Funcionario.objects.create(nome=nome, email=email, setor=setor)
-    pessoas = Funcionario.objects.all
-    return render(request, 'index.html', {'pessoas' : pessoas})
+    funcionario = Funcionario.objects.create(nome=nome, email=email, setor=setor)
+    
+    funcionario.save()
+    return redirect(listar)
 
 def editar(request, id):
     # pegar usuario do banco
