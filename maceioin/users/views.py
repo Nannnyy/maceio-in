0c68d6650f81c78f79a6ca1dfbc52551ee47cb61
Carol <anakfig2016@gmail.com>
2025-02-14 08:08:31 -0300

    error handling when logging in or registering a user and improving the appearance of inputs

--- maceioin/users/views.py
+++ maceioin/users/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login as auth_login
-
+from django.contrib import messages  # Certifique-se de que isso está importado
 from core.views import home as core_home
 
 
@@ -12,30 +11,41 @@ def cadastro(request):
         email = request.POST.get('email')
         senha = request.POST.get('password')
         
-        existing_user = User.objects.filter(username=username).first()
+        if not username or not email or not senha:
+            messages.error(request, "Todos os campos são obrigatórios!")
+            return render(request, 'cadastro.html')
         
-        if existing_user:
-            return HttpResponse('Usuário já existente')
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Usuário já existe!")
+            return render(request, 'cadastro.html')
         
+                # Criando usuário
         user = User.objects.create_user(username=username, email=email, password=senha)
         user.save()
-        
-        return redirect(login)
+
+        messages.success(request, "Cadastro realizado com sucesso! Faça login.")
+        return redirect("login")  # Redireciona para a tela de login
+
     return render(request, 'cadastro.html')
+
 
 
 def login(request):
     if request.method == "POST":
         username = request.POST.get('username')
         senha = request.POST.get('password')
-        
+
+        if not username or not senha:
+            messages.error(request, "Preencha todos os campos!")
+            return render(request, 'login.html')
+
         user = authenticate(username=username, password=senha)
-        
+
         if user:
             auth_login(request, user)
-            
             return redirect(core_home)
         else:
-            # Eu quero mudar isso aqui
-            return HttpResponse('Eamil ou senha invalido')
+            messages.error(request, "Usuário ou senha inválidos!")
+            return render(request, 'login.html')
+
     return render(request, 'login.html')
